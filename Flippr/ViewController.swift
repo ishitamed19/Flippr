e0a7da4f10b7173dc8018bf28187a5d057703f54
Ishita Mediratta <ishitamed19@yahoo.com>
2017-03-06 22:18:50 +0530

    Initial Commit

---
+++ Flippr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flippr
+//
+//  Created by Ishita Mediratta on 06/03/17.
+//  Copyright © 2017 Sudhir Mediratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
